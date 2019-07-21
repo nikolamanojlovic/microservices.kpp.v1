7e7d2ae819b3d04c06a62f06d1a8ee8aa0357aaa
nikolamanojlovic <manojlovic.nikola95@gmail.com>
2019-07-21 13:57:18 +0200

    typesafe

---
+++ servisi/veb-kpp/react-kpp/src/komponente/MeniForma.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class MeniForma extends React.Component {
+
+    render() {
+        return {
+            
+        }
+    }
+}
---
+++ servisi/veb-kpp/react-kpp/src/komponente/PrijavaForma.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export class PrijavaForma extends React.Component {
+
+    render() {
+        return {
+            
+        }
+    }
+}
