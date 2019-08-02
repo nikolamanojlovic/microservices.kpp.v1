30d53787e9b7961c259f95d9abd0c67e7d49a4c7
nmanojlovic <n.manojlovic@msgnetconomy.net>
2019-08-02 09:28:43 +0200

    persisting state

--- servisi/veb-kpp/react-kpp/src/pomocnici/Konstante.tsx
+++ servisi/veb-kpp/react-kpp/src/pomocnici/Konstante.tsx
@@ -1,6 +1,8 @@
 export const API_ZAPOSLENI: string = "http://0.0.0.0:3001/api";
 export const API_PROCESI: string = " http://kpp-mikroservis-procesi/api";
 
+export const STANJE: string = "kpp-stanje";
+
 export const TIP_PROCESA: string[] = ["Управљачки", "Кључни", "Помоћни"]
 export const TIP_DOKUMENTA: string[] = ["doc, docx", "pdf", "mp3", "avi", "mov"]
 
--- servisi/veb-kpp/react-kpp/src/store/konfiguracija.tsx
+++ servisi/veb-kpp/react-kpp/src/store/konfiguracija.tsx
@@ -2,6 +2,32 @@ import { korisnikReducer } from './korisnik/reducer';
 import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { KorisnikAkcije } from './korisnik/tipovi';
 import thunk, { ThunkMiddleware } from "redux-thunk";
+import {STANJE} from '../pomocnici/Konstante';
+
+function ucitajStanje(): StanjeAplikacije | undefined {
+    try {
+        const serializovanoStanje = localStorage.getItem(STANJE)
+
+
+        if (serializovanoStanje === null) {
+            return undefined;
+        }
+        return JSON.parse(serializovanoStanje);
+    } catch (error) {
+        console.log(error.message);
+        return undefined;
+    }
+}
+
+function sacuvajStanje(stanje: StanjeAplikacije): void {
+    try {
+        const serializovanoStanje = JSON.stringify(stanje);
+        localStorage.setItem(STANJE, serializovanoStanje)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
 
 // SVE AKCIJE KOJE APLIKACIJA IMA
 export type AkcijeAplikacije = KorisnikAkcije;
@@ -14,4 +40,8 @@ const reducerAplikacije = combineReducers({
 // SVA STANJA KOJE APLIKACIJA IMA
 export type StanjeAplikacije = ReturnType<typeof reducerAplikacije>
 
-export const store = createStore(reducerAplikacije, applyMiddleware(thunk as ThunkMiddleware<StanjeAplikacije, AkcijeAplikacije>));
+export const store = createStore(reducerAplikacije, ucitajStanje(), applyMiddleware(thunk as ThunkMiddleware<StanjeAplikacije, AkcijeAplikacije>));
+
+store.subscribe(() => {
+    sacuvajStanje(store.getState());
+});
