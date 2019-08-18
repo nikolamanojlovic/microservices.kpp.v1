45a4a6357549bc7c4357b3a08e288d135769aab5
nikolamanojlovic <manojlovic.nikola95@gmail.com>
2019-08-18 22:23:17 +0200

    Fe

--- servisi/veb-kpp/react-kpp/src/komponente/Aktivnost.tsx
+++ servisi/veb-kpp/react-kpp/src/komponente/Aktivnost.tsx
@@ -100,9 +100,12 @@ export class Aktivnost extends Component<Props, AktivnostStanje> {
                 {
                     this.props.aktivnost ? <span /> :
                         <div className="aktivnost-funkcionalnosti">
-                            <svg className="input" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._dodajGranjanje()}>
-                                <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z" />
-                            </svg>
+                            {
+                                this.props.tok.aktivnostiUToku.length < 2 ? <span /> :
+                                    <svg className="input" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._dodajGranjanje()}>
+                                        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 17h-2v-2h2v2zm2.07-7.75l-.9.92C13.45 12.9 13 13.5 13 15h-2v-.5c0-1.1.45-2.1 1.17-2.83l1.24-1.26c.37-.36.59-.86.59-1.41 0-1.1-.9-2-2-2s-2 .9-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 .88-.36 1.68-.93 2.25z" />
+                                    </svg>
+                            }
                             <svg className="input" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._sacuvajAktivnost()}>
                                 <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" />
                             </svg>
--- servisi/veb-kpp/react-kpp/src/komponente/Granjanje.tsx
+++ servisi/veb-kpp/react-kpp/src/komponente/Granjanje.tsx
@@ -9,8 +9,6 @@ interface GranjanjeProps {
 }
 
 interface GranjanjeStanje {
-    uslov: string,
-    usloviGranjanja: Array<IUslovTranzicije>,
     usloviElementi: Array<JSX.Element>
 }
 
@@ -19,33 +17,26 @@ type Props = GranjanjeProps;
 export class Granjanje extends Component<Props, GranjanjeStanje> {
 
     state: Readonly<GranjanjeStanje> = {
-        uslov: "",
-        usloviGranjanja: [{
-            rbTranzicije: 1,
-            izlazniProces: this.props.proces.idProcesa,
-            izlazniTok: this.props.tok.rbToka,
-            idIzlaza: -1,
-            rezultat: ""
-        }],
         usloviElementi: [
-            <div className="granjanje-uslovi-input">
-                <input className="input-tekst input-kreiraj input-granjanje-uslov granjanje-uslov-label-levo" name="uslov" type="text" onChange={(e: FormEvent<HTMLInputElement>) => this._obradiRezultatSledece(e)}/>
-                <select className="input-tekst input-kreiraj input-granjanje-uslov granjanje-uslov-label-desno" name="aktivnost" disabled={true}>
+            <form className="granjanje-uslovi-input">
+                <input className="input-tekst input-kreiraj input-granjanje-uslov" name="uslov" type="text" />
+                <select className="input-tekst input-kreiraj input-granjanje-uslov" name="aktivnost" disabled={true}>
                     <option key={-1} value={SELEDECA}>{SELEDECA}</option>
                 </select>
-            </div>,
-            <div className="granjanje-uslovi-input">
-                <input className="input-tekst input-kreiraj input-granjanje-uslov granjanje-uslov-label-levo" name="uslov" type="text" />
-                <select className="input-tekst input-kreiraj input-granjanje-uslov granjanje-uslov-label-desno" name="aktivnost" >
+            </form>,
+            <form className="granjanje-uslovi-input">
+                <input className="input-tekst input-kreiraj input-granjanje-uslov" name="uslov" type="text" />
+                <select className="input-tekst input-kreiraj input-granjanje-uslov" name="aktivnost" >
                     {
                         this._renderujOpcije()
                     }
                 </select>
-            </div>
+            </form>
         ]
     }
 
     _renderujOpcije(): Array<JSX.Element> {
+        console.log("fweionfowe")
         let opcije: Array<JSX.Element> = [];
 
         this.props.proces.tok.map((t) => {
@@ -85,23 +76,20 @@ export class Granjanje extends Component<Props, GranjanjeStanje> {
     }
 
     /********************** FUNKCIJE GRANJANJA **********************/
-
-    _postaviUslov(uslov: FormEvent<HTMLInputElement>) {
-        this.setState({ uslov: uslov.currentTarget.value })
-    }
-
-    _obradiRezultatSledece(e: FormEvent<HTMLInputElement>) {
-        let uslovi = this.state.usloviGranjanja;
-        uslovi[0].rezultat = e.currentTarget.value;
-
-        this.setState({ ...this.state, usloviGranjanja: uslovi });
-    }
-
     render() {
         return (
             <div className="granjanje-kontejner">
                 <div className="granjanje-sadrzaj">
                     <div className="granjanje-funkcionalnosti">
+                        <svg className="input input-granjanje-dodaj input-granjanje-funkcionalnosti" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._dodajJosUslovaGranjanja()}>
+                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
+                        </svg>
+                        {
+                            this.state.usloviElementi.length <= 2 ? <span /> :
+                                <svg className="input svg-obrisi input-granjanje-funkcionalnosti" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._obrisiUslovGranjanja()}>
+                                    <path d="M14.59 8L12 10.59 9.41 8 8 9.41 10.59 12 8 14.59 9.41 16 12 13.41 14.59 16 16 14.59 13.41 12 16 9.41 14.59 8zM12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z" />
+                                </svg>
+                        }
                         <svg className="input" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                             <path d="M17 3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V7l-4-4zm-5 16c-1.66 0-3-1.34-3-3s1.34-3 3-3 3 1.34 3 3-1.34 3-3 3zm3-10H5V5h10v4z" />
                         </svg>
@@ -109,22 +97,15 @@ export class Granjanje extends Component<Props, GranjanjeStanje> {
                             <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z" />
                         </svg>
                     </div>
-                    <div className="granjanje-uslovi-sadrzaj">
+                    <div className="granjanje-uslovi-uslov">
                         <label className="label-granjanje">Услов грањања процеса:</label>
-                        <input className="input-tekst input-kreiraj input-granjanje" name="uslov" type="text" value={this.state.uslov} onChange={(e: FormEvent<HTMLInputElement>) => this._postaviUslov(e)} />
+                        <input className="input-tekst input-kreiraj input-granjanje" name="uslov" type="text" />
+                    </div>
+                    <div className="granjanje-uslovi-sadrzaj">
                         {
                             this.state.usloviElementi.map(e => {
                                 return e;
                             })
-                        }
-                        <svg className="input input-granjanje-dodaj" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._dodajJosUslovaGranjanja()}>
-                            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11h-4v4h-2v-4H7v-2h4V7h2v4h4v2z" />
-                        </svg>
-                        {
-                            this.state.usloviElementi.length <= 2 ? <span /> :
-                                <svg className="input svg-obrisi" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" onClick={() => this._obrisiUslovGranjanja()}>
-                                    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm5 11H7v-2h10v2z" />
-                                </svg>
                         }
                     </div>
                 </div>
